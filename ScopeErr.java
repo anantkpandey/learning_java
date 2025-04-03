6735465f85b5a1f4cdc829c0b87245f07a0535cc
Anant Kumar Pandey <bdeanant.official@gmail.com>
2025-04-03 19:17:58 +0530

    Create ScopeErr.java

---
+++ ScopeErr.java
@@ -0,0 +1,9 @@
+// this program will not compile.
+class ScopeErr{
+  public static void main(String args[]){
+    int bar = 1;
+    {  // creates a new scope
+      int bar = 2;  // compile time error - bar already defined!
+    }
+  }
+}
